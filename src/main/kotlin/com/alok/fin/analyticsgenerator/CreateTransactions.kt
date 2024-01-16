b5ce257192269d7e7b6c85bab6cf7421fec8b30b
alokkulkarni <kulkarni.alok@gmail.com>
2024-01-16 19:14:39 +0000

    updated kafka connect Elastic search connector

--- src/main/kotlin/com/alok/fin/analyticsgenerator/CreateTransactions.kt
+++ src/main/kotlin/com/alok/fin/analyticsgenerator/CreateTransactions.kt
@@ -1,8 +1,11 @@
-@file:Suppress("UNUSED_VARIABLE", "UNUSED_PARAMETER")
+@file:Suppress("UNUSED_VARIABLE", "UNUSED_PARAMETER", "UNUSED_EXPRESSION")
 
 package com.alok.fin.analyticsgenerator
 
+import java.math.BigDecimal
+import java.time.Instant
 import java.util.*
+import kotlin.random.asKotlinRandom
 
 class CreateTransactions {
 
@@ -26,20 +29,21 @@ class CreateTransactions {
         val merchantCategoryCodes = merchantCategoryCodesRepository.findAll().randomOrNull()
         print (merchantCategoryCodes)
 
+
         val transactions = Transactions(
             UUID.randomUUID(),
-            accountId = "UUID.randomUUID().toString()",
-            customerId = UUID.randomUUID().toString(),
-            transactionType = "transactionType",
-            transactionDate = "transactionDate",
-            transactionAmount = "transactionAmount",
-            transactionCurrency = "transactionCurrency",
-            transactionRefrence = "transactionRefrence" + Random().nextInt().toString(),
-            transactionStatus = "transactionStatus",
-            transactionCategory = merchantCategoryCodes?.categoryDescription ?: "transactionCategory",
-            transactionSubCategory = "transactionSubCategory",
-            transactionLatitude = "transactionLatitude",
-            transactionLongitude = "transactionLongitude",
+            accountId = arrayOf("20245687665544", "20224599443322" ).random().toString(),
+            customerId = arrayOf("7654321892", "8765434678" ).random().toString(),
+            transactionType = arrayOf("debit", "credit" ).random().toString(),
+            transactionDate = Instant.now().toString(),
+            transactionAmount = BigDecimal.valueOf(Random().asKotlinRandom().nextDouble(100.0, 1000.0)).toString(),
+            transactionCurrency = "GBP",
+            transactionRefrence = arrayOf("transactionReference1", "transactionReference2" ).random().toString(),
+            transactionStatus = arrayOf( "posted", "pending", "Declined" ).random().toString(),
+            transactionCategory = arrayOf( "Food & Dining", "Entertainment","Education", "Shopping","Personal Care","Health & Fitness" ).random().toString(),
+            transactionSubCategory = arrayOf( "Catering", "Coffee shops","Delivery", "Charity","Movies & DVDs","Games" ).random().toString(),
+            transactionLatitude = arrayOf( "51.5072° N", "40.4168° N" ).random().toString(),
+            transactionLongitude = arrayOf( "0.1275° W", "3.7038° W" ).random().toString(),
             merchantId = merchantCategoryCodes?.id
         )
         val save = transactionsRepository.save(transactions)
